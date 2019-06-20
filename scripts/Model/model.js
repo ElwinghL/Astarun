c05b3f7020bb0d39baaa713832c5ff5d940c7e13
ElwinghL <clement.leboeuf@edu.univ-fcomte.fr>
2019-06-20 23:42:55 +0200

    Mise à jour des commentaires

--- scripts/Model/model.js
+++ scripts/Model/model.js
@@ -118,6 +118,14 @@ class Jauge {
 }
 
 class Stat {
+    /**
+     * Stat physique ou mentale du personnage
+     * @param {String} nom Nom de la stat
+     * @param {Number} valeur Valeur de base obtenue au dé
+     * @param {Number} rBonus Bonus racial
+     * @param {Number} dBonus Bonus définitif
+     * @param {Number} sBonus Bonus du stuff
+     */
     constructor(nom, valeur, rBonus = 0, dBonus = 0, sBonus = 0) {
         this.nom = nom;
         this.valeur = valeur;
@@ -126,17 +134,28 @@ class Stat {
         this.points = this.calcPoints(dBonus);
         this.mod = this.calcMod();
     }
-
+    /**
+     * Coup en points de la stat
+     * @param {Number} dBonus 
+     */
     calcPoints(dBonus) {
-        return dBonus * 30;
+        return dBonus * (dBonus < 0 ? 10 : 30);
     }
-
+    /**
+     * Calcul du modificateur de la stat
+     */
     calcMod() {
         return Math.ceil(this.total > 17 ? this.total - 17 + 4 : (this.total - 10) / 2)
     }
 }
 
 class Resistance {
+    /**
+     * Resistance à un élément du personnage
+     * @param {String} elem Element de la résistance
+     * @param {Number} valeur Resistance de base
+     * @param {Number} niveau Niveau de la résistance
+     */
     constructor(elem, valeur, niveau) {
         this.elem = elem;
         this.valeur = valeur;
@@ -152,13 +171,22 @@ class Resistance {
     calcPoints() {
         return defPoints(this.niveau);
     }
-
+    /**
+     * Calcul le montant total de la résistance
+     */
     calcTotal() {
         return this.valeur + 5 * this.niveau;
     }
 }
 
 class Connaissance {
+    /**
+     * Classe abstraite de gestion des connaissances
+     * @param {String} nom Nom de la connaissance
+     * @param {Number} niveau Niveau de la connaissance
+     * @param {Number} calcPoints Cout en point de la connaissance
+     * @param {String} description Description de la connaissance
+     */
     constructor(nom, niveau, calcPoints, description = "") {
         this.nom = nom;
         this.niveau = niveau;
@@ -169,12 +197,26 @@ class Connaissance {
 }
 
 class Langue extends Connaissance {
+    /**
+     * Langue connue par le personnage
+     * @param {String} nom Nom de la langue connue
+     * @param {Number} points Cout en point de la langue apprise
+     */
     constructor(nom, points) {
         super(nom, 0, points);
     }
 }
 
 class Competence extends Connaissance {
+    /**
+     * 
+     * @param {String} nom Nom de la compétence
+     * @param {Number} niveau Niveau de la compétence
+     * @param {String} description Description de la compétence
+     * @param {Boolean} bPalier Est-ce une compétence de palier ?
+     * @param {Stat} stat1 Sinon stat principale
+     * @param {Stat} stat2 Sinon stat secondaire
+     */
     constructor(nom, niveau, description = "", bPalier = true, stat1 = null, stat2 = null) {
         super(nom, niveau, defPoints(niveau), description);
         this.bPalier = bPalier;
@@ -184,7 +226,9 @@ class Competence extends Connaissance {
         this.mod = this.calcMod();
         this.total = calcTotal();
     }
-
+    /**
+     * Calcul le modificateur de la compétence
+     */
     calcMod() {
         if (this.bPalier) {
             return (0 + (this.stat1 ? this.stat1.mod : 0) + Math.ceil(this.stat2 ? this.stat2.mod / 2 : 0))
@@ -192,12 +236,25 @@ class Competence extends Connaissance {
             return 0;
         }
     }
+    /**
+     * Calcul le total de la compétence à ajouter au dé
+     */
     calcTotal() {
         return (this.bPalier ? this.mod + this.niveau * 2 : 0);
     }
 }
 
 class Stance extends Connaissance {
+    /**
+     * Stance de combat utilisée par le personnage
+     * @param {String} nom Nom de la stance de combat
+     * @param {Number} niveau Niveau de la stance de combat
+     * @param {Number} prec Précision de la stance de combat à ajouter au dé
+     * @param {Number} mAtk Modificateur d'attaque pour les dégats
+     * @param {Number} crit Seuil à dépasser pour faire un coup critique
+     * @param {Numer} mCrit Multiplicateur de dégats critiques
+     * @param {String} description Description de la stance de combat
+     */
     constructor(nom, niveau, prec, mAtk, crit, mCrit, description = "") {
         super(nom, niveau, defPoints(niveau), description);
         this.prec = prec;
@@ -208,12 +265,25 @@ class Stance extends Connaissance {
 }
 
 class Metier extends Connaissance {
+    /**
+     * Métier appris par le personnage
+     * @param {String} nom Nom du métier du personnage
+     * @param {Number} niveau Niveau du métier
+     * @param {String} description Description du métier
+     */
     constructor(nom, niveau, description = "") {
         super(nom, niveau, defPoints(niveau) * 2, description);
     }
 }
 
 class Magie extends Connaissance {
+    /**
+     * Sort ou école de Magie apprise par le personnage
+     * @param {String} nom Nom du sort ou de l'école de magie apprise
+     * @param {Number} niveau Niveau du sort ou de l'école
+     * @param {String} elem Element du sort ou de l'école
+     * @param {String} description Description de l'école ou du sort
+     */
     constructor(nom, niveau, elem, description = "") {
         super(nom, niveau, defPoints(niveau) * 2, description);
         this.elem = elem;
